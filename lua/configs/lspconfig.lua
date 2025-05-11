e3cc8bddad80cd871d648f4e29891051f52498cb
ericsmacedo <34794915+ericsmacedo@users.noreply.github.com>
2025-05-11 22:20:19 +0100

    Adds lspconfig and options

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,29 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = { "pylsp", "jedi_language_server", "verible"}
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+--
+--
+vim.lsp.config('pylsp', {
+  cmd = {vim.call("expand", "~/.config/nvim/env/bin/pylsp")},
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'E226'},
+          maxLineLength = 88
+        }
+      }
+    }
+  }
+})
+
+vim.lsp.config('jedi_language_server', {
+  cmd = {vim.call("expand", "~/.config/nvim/env/bin/jedi-language-server")},
+})
+
+
+vim.lsp.enable(servers)
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,54 @@
 require "nvchad.options"
 
--- add yours here!
+-- Vim has 3 kinds of options: global, buffer-local and window-local
+local o = vim.o  	-- Global options
+local wo = vim.wo	-- window-local options
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- o.wildignore = "+=*/tmp/*,*.so,*.swp,*.zip,"   -- MacOSX/Linux
+o.scrolloff = 5       -- Show at least 5 lines
+o.cursorlineopt ='both' -- to enable cursorline!
+
+-- window-local options
+wo.relativenumber = true	-- use relative numbers
+wo.colorcolumn = "88"		  -- columnt to indicate end of 80 characters
+wo.conceallevel = 0       -- Determine how text with the "conceal" syntax attribute :syn-conceal is shown
+wo.foldmethod = 'marker'
+
+-- buffer local options
+o.softtabstop = 4         -- number of spaces in tab when editing.
+o.tabstop = 4             -- number of spaces in tab when editing
+o.expandtab = true        -- tabs are spaces
+o.shiftwidth = 4          -- number of spaces used for >> and << 
+
+-- -- To prevent errors with the Shada (shared data) file, I added the line below
+-- vim.cmd[[ "set viminfo+=n~/vim/viminfo"]]
+-- 
+-- Specify which python environment is to be used with neovim
+vim.g.loaded_python3_provider = 1
+vim.g.python3_host_prog = vim.call("expand", "~/.config/nvim/env/bin/python")
+
+-- "===================================================================
+-- " CUSTOM INDENTATION
+-- "===================================================================
+local set_ident = function()
+    vim.o.tabstop = 4          -- number of spaces in tab when editing
+    vim.o.softtabstop = 4      -- number of spaces in tab when editing.
+    vim.o.shiftwidth = 4       -- number of spaces used for >> and <<
+    vim.o.expandtab = true     -- tabs are spaces
+end
+
+local set_yml_ident = function()
+    vim.o.tabstop = 2          -- number of spaces in tab when editing
+    vim.o.softtabstop = 2      -- number of spaces in tab when editing.
+    vim.o.shiftwidth = 2       -- number of spaces used for >> and <<
+end
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"},
+                            {pattern = {"*.py", "*.sv", "*.cpp"},
+                             callback = set_ident
+                            })
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"},
+                            {pattern = {"*.yml"},
+                             callback = set_yml_ident
+                            })
