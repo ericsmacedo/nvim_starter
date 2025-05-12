813d106bb789872a96ba2f512750297db6735a29
ericsmacedo <34794915+ericsmacedo@users.noreply.github.com>
2025-05-12 11:36:38 +0100

    Add more plugins to neovim
    Adds the tabular plugin + a gitignore to neovim + requirements txt file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,21 @@ return {
       require "configs.lspconfig"
     end,
   },
+    
+  -- Plugin used to align text inside neovim
+  {"godlygeek/tabular"},
+
+  {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {},
+      ft = 'markdown'
+  
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
